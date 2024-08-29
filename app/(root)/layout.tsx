72f921dd8d0fd99fc57d1febf4217a8b3a3c2541
Imbalancement <coreywilliams733@gmail.com>
2024-08-28 20:08:02 -0400

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,25 @@
+import Sidebar from "@/components/Sidebar"
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: "Corey", lastName: "W"}
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}/>
+
+      <div className="flex size-full flex-col">
+      <div className="root-layout">
+
+      <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+      </div>
+      <MobileNav user = {loggedIn} />
+      </div>
       {children}
     </main>
   )
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSideBar from '@/components/RightSideBar';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Corey', lastName: "W", email:"coreywilliams733@gmail.com" };
+
+
   return (
-    <div> Home</div>
+    <section className='home'> 
+    <div className='home-content'>
+      <header className='home-header'>
+
+       <HeaderBox 
+       type="greeting"
+       title="Welcome"
+       user={loggedIn?.firstName || 'Guest'}
+       subtext="Access and mange your account and
+       transactions efficiently"
+       
+       />
+      <TotalBalanceBox 
+      accounts={[]}
+      totalBanks={1}
+      totalCurrentBalance={1250.35}
+      />
+
+      </header>
+
+      RECENT TRANSACTIONS
+    </div>
+
+    <RightSideBar 
+    user={loggedIn}
+    transactions={[]}
+    banks={[{}, {}]}
+    />
+    </section>
   )
 }
 
